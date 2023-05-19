f88bc86620a53d1fec919704867284887a449da4
Merajul Islam <meraj.exe@gmail.com>
2023-05-19 22:05:00 +0600

    single id query api finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,13 @@ async function run() {
     app.get('/gallery', async (req, res) => {
       const toys = await toyCollection.find().limit(6).toArray();
       res.send(toys);
+    });
+
+    app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection

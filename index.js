af5f27b604a3d50933bfaf97eb90025da0c87b5c
Merajul Islam <meraj.exe@gmail.com>
2023-05-20 05:10:18 +0600

    delete api set

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ async function run() {
       const result = await toyCollection.insertOne(toy);
       res.send(result);
     });
+
+    app.delete('/mytoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
